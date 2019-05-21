9b4d4663d5966d39aa3e3131a71a24c8bb3db03d
Zenna Tavares <zennatavares@gmail.com>
2019-05-21 14:11:26 -0400

    truncate

--- src/plot.jl
+++ src/plot.jl
@@ -1,5 +1,5 @@
 "Line Plot of  with UnicodePlots"
-function plotscalar(; title = "", display_ = display, LTX::Type{TX} = Float64, LTY::Type{TY} = Float64, lineplotkwargs...) where {TX, TY}
+function plotscalar(; title = "", display_ = display, LTX::Type{TX} = Float64, LTY::Type{TY} = Float64, truncate = true, lineplotkwargs...) where {TX, TY}
   xs = TX[]
   ys = TY[]
   maxseen = typemin(TY)
@@ -8,6 +8,9 @@ function plotscalar(; title = "", display_ = display, LTX::Type{TX} = Float64, L
   function innerplotscalar((x, y))
     push!(xs, x)
     push!(ys, y)
+    if truncate && !(-1e5 < y < 1e5)
+      return nothing
+    end
     if !isempty(xs)
       display_(UnicodePlots.lineplot(xs, ys; title = title, lineplotkwargs...))
     end
